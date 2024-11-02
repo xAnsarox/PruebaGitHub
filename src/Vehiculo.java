a43a5dad76dae22ccff31a4c8698114b3abde274
AntonioSalazar <antonio.salazar@live.u-tad.com>
2024-11-02 13:19:21 +0100

    Funcionalidad tlf1 ok

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,10 +1,13 @@
 
 public class Vehiculo {
 
-	public static void main(String[] args) {
+	public static void arrancar() {
 
 		System.out.print("Brrrrr... acaba de arrancar!");
 
+	}
+	public static void main (String [] args) {
+		arrancar ();
 	}
 
 }
